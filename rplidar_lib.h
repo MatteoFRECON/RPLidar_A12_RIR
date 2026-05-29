7cea987e75ecf356bb70ea57743aa0101d80247e
Matteo FRECON <matteo.frecon@gmail.com>
2026-05-29 16:25:07 +0200

    Update rplidar_lib.h
    Commentaire

--- rplidar_lib.h
+++ rplidar_lib.h
@@ -1,7 +1,25 @@
+/*
+  rplidar_lib.h
+  -----------------------------------------------------------------------------
+  Déclaration de la classe utilisée pour piloter un RPLIDAR depuis un ESP32/Arduino.
+
+  Ce fichier ne contient pas la logique détaillée : il décrit seulement les fonctions
+  disponibles pour le programme principal. L'implémentation se trouve dans
+  rplidar_lib.cpp.
+
+  Principe général :
+  - un port série Arduino Stream est utilisé pour échanger les octets avec le LIDAR ;
+  - une broche PWM commande le moteur du capteur ;
+  - les fonctions publiques permettent d'initialiser, commander, lire et décoder le LIDAR.
+*/
+
 #ifndef rplidar_lib_h
 #define rplidar_lib_h
 
 #include "Arduino.h"
+
+// La classe utilise Stream afin de rester indépendante du port série choisi.
+// Dans ce projet, le programme principal lui passe Serial2.
 
 // Définition de la classe rplidar_lib pour interagir avec un capteur RPLIDAR
 class rplidar_lib {
@@ -27,6 +45,9 @@ public:
   void getconf();                  // Récupère la configuration du LIDAR
   void getinfo();                  // Récupère les infos générales (version, modèle...)
   
+// Une mesure standard de scan RPLIDAR est codée sur 5 octets.
+// Cette méthode transforme la trame brute en valeurs physiques exploitables.
+
   // Traite une mesure : extrait qualité, angle et distance depuis une trame brute de 5 octets
   int process_scan(const byte hit[5], int& new_scan, int& quality, float& angle, float& distance);
 
